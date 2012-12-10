084a4c7f232d4c23485fb82cddad56595369b38d
John Jeffery <jjeffery@sp.com.au>
2012-12-11 07:44:27 +1000

    added a test for memory queue storage

---
+++ memory_queue_test.go
@@ -0,0 +1,64 @@
+package stomp
+
+import (
+	. "launchpad.net/gocheck"
+	"github.com/jjeffery/stomp/message"
+)
+
+type MemoryQueueSuite struct{}
+
+var _ = Suite(&MemoryQueueSuite{})
+
+func (s *MemoryQueueSuite) Test1(c *C) {
+	mq := NewMemoryQueueStorage()
+	mq.Start()
+	
+	f1 := message.NewFrame(message.MESSAGE,
+		message.Destination, "/queue/test",
+		message.MessageId, "msg-001",
+		message.Subscription, "1")
+		
+	err := mq.Enqueue("/queue/test", f1)
+	c.Assert(err, IsNil)
+	
+	f2 := message.NewFrame(message.MESSAGE,
+		message.Destination, "/queue/test",
+		message.MessageId, "msg-002",
+		message.Subscription, "1")
+		
+	err = mq.Enqueue("/queue/test", f2)
+	c.Assert(err, IsNil)
+	
+	f3 := message.NewFrame(message.MESSAGE,
+		message.Destination, "/queue/test2",
+		message.MessageId, "msg-003",
+		message.Subscription, "2")
+		
+	err = mq.Enqueue("/queue/test2", f3)
+	c.Assert(err, IsNil)
+	
+	// attempt to dequeue from a different queue
+	f, err := mq.Dequeue("/queue/other-queue")
+	c.Check(err, IsNil)
+	c.Assert(f, IsNil)
+	
+	f, err = mq.Dequeue("/queue/test2")
+	c.Check(err, IsNil)
+	c.Assert(f, Equals, f3)
+	
+	f, err = mq.Dequeue("/queue/test")
+	c.Check(err, IsNil)
+	c.Assert(f, Equals, f1)
+	
+	f, err = mq.Dequeue("/queue/test")
+	c.Check(err, IsNil)
+	c.Assert(f, Equals, f2)
+	
+	f, err = mq.Dequeue("/queue/test")
+	c.Check(err, IsNil)
+	c.Assert(f, IsNil)
+	
+	f, err = mq.Dequeue("/queue/test2")
+	c.Check(err, IsNil)
+	c.Assert(f, IsNil)	
+}
---
+++ stomp_test.go
@@ -0,0 +1,12 @@
+package stomp
+
+import (
+	"launchpad.net/gocheck"
+	"testing"
+)
+
+// Runs all gocheck tests in this package.
+// See other *_test.go files for gocheck tests.
+func TestStomp(t *testing.T) {
+	gocheck.TestingT(t)
+}
