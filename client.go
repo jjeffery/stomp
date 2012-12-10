f33a4c0033a9d9a1db49f5a61dd08b05f9f7be61
John Jeffery <jjeffery@sp.com.au>
2012-12-11 08:00:44 +1000

    started on what the client API will look like, but no implementation as yet

--- client.go
+++ client.go
@@ -1,3 +1,50 @@
 package stomp
 
-// reserved for STOMP client operations
+import (
+	"github.com/jjeffery/stomp/message"
+)
+
+// Indicates the acknowledgement mode for a STOMP subscription.
+type AckMode int
+
+// AckMode constants
+const (
+	AckAuto AckMode = iota
+	AckClient
+	AckClientIndividual
+)
+
+// A Client is a STOMP client.
+type Client struct {
+	// TODO
+}
+
+type Subscription struct {
+	C chan *message.Frame
+	// TODO other members: client, id, etc
+}
+
+func (s *Subscription) Unsubscribe() error {
+	panic("not implemented")
+}
+
+func NewClient(addr string) *Client {
+	panic("not implemented")
+}
+
+func (c *Client) Connect() error {
+	panic("not implemented")
+}
+
+func (c *Client) ConnectAuth(login, passcode string) error {
+	panic("not implemented")
+}
+
+func (c *Client) Disconnect() error {
+	panic("not implemented")
+}
+
+// Subscribe to a destination. Returns a channel for receiving message frames.
+func (c *Client) Subscribe(destination string, ack AckMode) (*Subscription, error) {
+	panic("not implemented")
+}
