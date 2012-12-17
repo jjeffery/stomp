e767e24907bd606ee1685dcf561efb16f40ee616
John Jeffery <jjeffery@sp.com.au>
2012-12-18 08:42:12 +1000

    Update server/client/client.go
    Minor update to comment.

--- server/client/client.go
+++ server/client/client.go
@@ -1,6 +1,7 @@
 /*
 This package implements key abstractions
 in the STOMP server. In particular the
-abstractions of a connection and a subscription.
+abstractions of a connection, a subscription,
+and a request.
 */
 package client
