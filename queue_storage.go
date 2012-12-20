04d886e58e2a1b8d990b64c55fb9663b2e5b60c8
John Jeffery <jjeffery@sp.com.au>
2012-12-20 14:33:33 +1000

    commented out unused errors, split MemoryQueueStorage into a separate file

--- queue_storage.go
+++ queue_storage.go
@@ -1,7 +1,6 @@
 package stomp
 
 import (
-	"container/list"
 	"github.com/jjeffery/stomp/message"
 )
 
@@ -32,67 +31,4 @@ type QueueStorage interface {
 	// Called prior to server shutdown. Allows the queue storage
 	// to perform any cleanup.
 	Stop()
-}
-
-// In-memory implementation of the QueueStorage interface.
-type MemoryQueueStorage struct {
-	lists map[string]*list.List
-}
-
-func NewMemoryQueueStorage() QueueStorage {
-	m := new(MemoryQueueStorage)
-	return m
-}
-
-func (m *MemoryQueueStorage) Enqueue(queue string, frame *message.Frame) error {
-	l, ok := m.lists[queue]
-	if !ok {
-		l = list.New()
-		m.lists[queue] = l
-	}
-	l.PushBack(frame)
-
-	return nil
-}
-
-// Pushes a frame to the head of the queue. Sets
-// the "message-id" header of the frame if it is not
-// already set.
-func (m *MemoryQueueStorage) Requeue(queue string, frame *message.Frame) error {
-	l, ok := m.lists[queue]
-	if !ok {
-		l = list.New()
-		m.lists[queue] = l
-	}
-	l.PushFront(frame)
-
-	return nil
-}
-
-// Removes a frame from the head of the queue.
-// Returns nil if no frame is available.
-func (m *MemoryQueueStorage) Dequeue(queue string) (*message.Frame, error) {
-	l, ok := m.lists[queue]
-	if !ok {
-		return nil, nil
-	}
-
-	element := l.Front()
-	if element == nil {
-		return nil, nil
-	}
-
-	return l.Remove(element).(*message.Frame), nil
-}
-
-// Called at server startup. Allows the queue storage
-// to perform any initialization.
-func (m *MemoryQueueStorage) Start() {
-	m.lists = make(map[string]*list.List)
-}
-
-// Called prior to server shutdown. Allows the queue storage
-// to perform any cleanup.
-func (m *MemoryQueueStorage) Stop() {
-	m.lists = nil
 }
