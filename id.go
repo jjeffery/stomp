cfc6b95f9b874aa086f2d5266b593a6ceee5f903
John Jeffery <jjeffery@sp.com.au>
2013-01-06 10:54:02 +1000

    add comments and examples

--- id.go
+++ id.go
@@ -5,16 +5,12 @@ import (
 	"sync/atomic"
 )
 
-// BUG(jpj): All unique identifiers for receipt, subscriptions
-// and transactions are allocated from the same namespace, and
-// are shared between clients. This could give a hostile STOMP
-// server information about the client.
-
 var _lastId uint64
 
 // allocateId returns a unique number for the current
 // process. Starts at one and increases. Used for 
-// allocating subscription ids, receipt ids, etc.
+// allocating subscription ids, receipt ids, 
+// transaction ids, etc.
 func allocateId() string {
 	id := atomic.AddUint64(&_lastId, 1)
 	return strconv.FormatUint(id, 10)
