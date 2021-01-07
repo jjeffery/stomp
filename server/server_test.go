23a46f5e7315e63e976bbc906c73d196d8e356d3
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2021-01-07 16:29:19 +0100

    Add test case

--- server/server_test.go
+++ server/server_test.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"runtime"
 	"testing"
+	"time"
 
 	"github.com/go-stomp/stomp"
 	. "gopkg.in/check.v1"
@@ -43,6 +44,37 @@ func (s *ServerSuite) TestConnectAndDisconnect(c *C) {
 	c.Assert(err, IsNil)
 
 	conn.Close()
+}
+
+
+func (s *ServerSuite) TestHeartBeatingTolerance(c *C) {
+	// Heart beat should not close connection exactly after not receiving message after cx
+	//  it should add a pretty decent amount of time to counter network delay of other timing issues
+	addr := ":59092"
+	l, err := net.Listen("tcp", addr)
+	c.Assert(err, IsNil)
+	defer func() { l.Close() }()
+	serv := Server{
+		Addr:          "",
+		Authenticator: nil,
+		QueueStorage:  nil,
+		HeartBeat:     5 * time.Millisecond,
+	}
+	go serv.Serve(l)
+
+	conn, err := net.Dial("tcp", "127.0.0.1"+addr)
+	c.Assert(err, IsNil)
+	defer conn.Close()
+
+	client, err := stomp.Connect(conn, stomp.ConnOpt.HeartBeat(5 * time.Millisecond, 5 * time.Millisecond))
+	c.Assert(err, IsNil)
+	defer client.Disconnect()
+
+	time.Sleep(serv.HeartBeat * 50) // let it go for some time to allow client and server to exchange some heart beat
+
+	// Ensure the server has not closed his readChannel
+	err = client.Send("/topic/whatever", "text/plain", []byte("hello"))
+	c.Assert(err, IsNil)
 }
 
 func (s *ServerSuite) TestSendToQueuesAndTopics(c *C) {
